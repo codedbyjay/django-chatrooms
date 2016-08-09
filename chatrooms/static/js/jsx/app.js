9bd9dd7056e9a78ec14ca48bd71b4ff83bfd7926
jaywhy13 <jeanmark.wright@gmail.com>
2016-08-08 23:28:20 -0500

    Added main js for app

---
+++ chatrooms/static/js/jsx/app.js
@@ -0,0 +1,68 @@
+import { Router, Route, Link, browserHistory } from 'react-router'
+
+
+var ChatMessage = React.createClass({
+    render: function(){
+        return (
+            <div className="chat-message">
+                {this.props.user}:
+                {this.props.text}
+            </div>
+        );
+    }
+});
+
+var ChatMessages = React.createClass({
+    render: function(){
+        var messageNodes = this.props.messages.map(function(message){
+            return (
+                <ChatMessage user={message.user} text={message.text} />
+            )
+        });
+        return (
+            <div className="chat-messages">
+                {messageNodes}
+            </div>
+        )
+    }
+});
+
+var sampleMessages = [
+    {user: "jay", text: "blah"},
+    {user: "jon", text: "blah"},
+];
+
+var ChatRoom = React.createClass({
+    render: function(){
+        return (
+            <div className="chatroom">
+            Chatroom goes here
+            <ChatMessages messages={sampleMessages} />
+            </div>
+        );
+    }
+});
+
+var App = React.createClass({
+    render: function(){
+        return (
+            <div>
+                <h1>Main window</h1>
+                <div>
+                    Options to create a room go here
+                </div>
+            </div>
+        )
+    }
+});
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+        <Route path="/" component="{ChatRoom}" />
+    </Router>
+), document.getElementById("container"))
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById("container")
+// )
