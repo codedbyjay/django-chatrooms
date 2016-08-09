13db3fb0cc2326a96e18313c8c0fdf2a35f1b5c7
jaywhy13 <jeanmark.wright@gmail.com>
2016-08-08 23:31:36 -0500

    Added view sets, WIP

--- chatrooms/views.py
+++ chatrooms/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
+from django.db.models import Q
 
 from registration.backends.simple.views import RegistrationView
+from rest_framework import viewsets
+
 from chatrooms.forms import LoginForm, RegistrationForm
+from chatrooms.serializers import *
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
@@ -35,3 +39,24 @@ class RegisterView(RegistrationView):
 
     form_class = RegistrationForm
     template_name = "chatrooms/register.html"
+
+
+class ChatRoomViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ChatRoomSerializer
+    queryset = ChatRoom.objects.all()
+
+    # def get_queryset(self):
+    #     return ChatRoom.objects.filter(
+    #         Q(users__contains=self.request.user) | 
+    #         Q(team__users__contains=self.request.user))
+
+class ChatMessageViewSet(viewsets.ModelViewSet):
+
+    serializer_class = ChatMessageSerializer
+    queryset = ChatMessage.objects.all()
+
+    # def get_queryset(self):
+    #     return ChatMessage.objects.filter(
+    #         Q(room__users__contains=self.request.user) | 
+    #         Q(room__team__users__contains=self.request.user))
